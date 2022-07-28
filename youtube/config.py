ea7a1c72452862dfc55e0c1b5f28e3edecbdbc3f
Kazi Ejaj <kejaj.777@gmail.com>
2022-07-29 01:35:41 +0600

    added env example

--- youtube/config.py
+++ youtube/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from apiclient import discovery
 
 load_dotenv()
-api_key = os.getenv('API_KEY')
+api_key = os.getenv('YOUTUBE_API_KEY')
 
 youtube = discovery.build('youtube', 'v3', developerKey=api_key)
 
